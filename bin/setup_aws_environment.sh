cbe9031a6099bcb9458c3fb7537e5c296d2c753e
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-18 00:33:25 +0200

    Create setup_aws_environment.sh

---
+++ bin/setup_aws_environment.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ex
+
+export AWS_SHARED_CREDENTIALS_FILE=/tmp/.aws
+mkdir -p /tmp
+touch ${AWS_SHARED_CREDENTIALS_FILE}
+
+cat > ${AWS_SHARED_CREDENTIALS_FILE} << EOF
+[master]
+aws_access_key_id=${AWS_ACCESS_KEY}
+aws_secret_access_key=${AWS_SECRET_KEY}
+region=${AWS_REGION}
+[dev]
+role_arn=${AWS_CROSS_ACCOUNT_ROLE_ARN}
+source_profile=master
+region=${AWS_REGION}
+EOF
+
+python3 -m ensurepip
