c9d65373152d2940b62f6e74aa36fa2227ef2e7c
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-18 00:34:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region                    = "eu-west-1"
-  shared_credentials_file   = "~/.aws/credentials"
+  shared_credentials_file   = "/tmp/.aws"
   profile                   = "dev"
 }
 
