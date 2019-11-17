1127f8495d7062ac19a39b31d497159f80277870
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-18 00:35:32 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "s3" {
     encrypt = true
-    bucket = "my-terraform-remote-state-storage-dev"
+    bucket = "rb-terraform-remote-state-storage-dev"
     key = "terraform/state/s3/python3-layer-lambda.tfstate"
     region = "eu-west-1"
     profile = "dev"
-    shared_credentials_file = "/Users/ruan/.aws/credentials"
+    shared_credentials_file = "/tmp/.aws"
     dynamodb_table = "terraform-state-lock"
   }
 }
