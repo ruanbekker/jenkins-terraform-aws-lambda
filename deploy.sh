4cb94145282173c89b6c29140794d49d48b50187
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-19 14:34:10 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
-export AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE:-/tmp/.aws}
-export AWS_PROFILE=dev
+#export AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE:-/tmp/.aws}
+export AWS_PROFILE=${AWS_PROFILE}
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
+export AWS_ROLE_ARN=${AWS_CROSS_ACCOUNT_ROLE_ARN}
+export AWS_ROLE_SESSION_NAME=dev
 
 # test 
 aws --profile dev sts get-caller-identity
@@ -10,5 +14,5 @@ pushd lambda
 popd
 
 rm -rf ~/.terraform
-AWS_SHARED_CREDENTIALS_FILE=/tmp/.aws AWS_PROFILE=dev TF_LOG=DEBUG terraform init
+TF_LOG=DEBUG terraform init
 terraform apply -auto-approve
