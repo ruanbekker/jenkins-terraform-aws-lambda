8d07df90c142a6e589f76ceeba9686f36b036a86
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-19 10:05:41 +0200

    Update build.sh

--- lambda/build.sh
+++ lambda/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 export WORKDIR=$(pwd)
 export LAYERSDIR="layer-requests"
 
@@ -8,6 +9,10 @@ mkdir -p packages/
 # build python-requests layer
 cd ${WORKDIR}/${LAYERSDIR}/
 ${WORKDIR}/${LAYERSDIR}/build_layer.sh
+
+export PKGDIR="python"
+rm -rf ${PKGDIR} && mkdir -p ${PKGDIR}
+pip3 install -r requirements.txt --no-deps -t ${PKGDIR}
+
 zip -r ${WORKDIR}/packages/python3-requests.zip .
 rm -rf ${WORKDIR}/${LAYERSDIR}/python/
-
