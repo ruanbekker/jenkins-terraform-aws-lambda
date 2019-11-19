311d8f97a3eea3386a03eb8e2f7349bb5a413cb6
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-19 14:46:54 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
 export AWS_ROLE_ARN=${AWS_CROSS_ACCOUNT_ROLE_ARN}
 export AWS_ROLE_SESSION_NAME=dev
+export AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE:-/tmp/.aws}
 
 # test 
 aws --profile dev sts get-caller-identity
