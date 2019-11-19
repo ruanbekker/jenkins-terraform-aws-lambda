1d81701c2ff21aa5fba435759b7fcf67e40865b8
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-19 10:06:12 +0200

    Update build_layer.sh

--- lambda/layer-requests/build_layer.sh
+++ lambda/layer-requests/build_layer.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-
+set -ex
 export PKGDIR="python"
 rm -rf ${PKGDIR} && mkdir -p ${PKGDIR}
 
-docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
-    pip3 install -r requirements.txt --no-deps -t ${PKGDIR}
+#docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.7 \
+virtualenv .venv -p /usr/local/bin/python3
+source .venv/bin/activate
+pip3 install -r requirements.txt --no-deps -t ${PKGDIR}
 
+deactivate .venv
+rm -rf .venv
