938ddc6fa5165734da1555a0173c06662c37e9a4
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-19 10:39:21 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 export AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE:-/tmp/.aws}
 
+# test 
+aws --profile dev sts get-caller-identity
+
 pushd lambda
 ./build.sh
 popd
