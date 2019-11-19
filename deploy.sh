a6aa5845521a83335bede44e2db4e0e24aa27299
Ruan Bekker <ruan.ru.bekker@gmail.com>
2019-11-19 14:44:19 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 #export AWS_SHARED_CREDENTIALS_FILE=${AWS_SHARED_CREDENTIALS_FILE:-/tmp/.aws}
+unset AWS_PROFILE
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+unset AWS_ROLE_ARN
+unset AWS_ROLE_SESSION_NAME
 export AWS_PROFILE=${AWS_PROFILE}
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
@@ -8,7 +13,7 @@ export AWS_ROLE_SESSION_NAME=dev
 
 # test 
 aws --profile dev sts get-caller-identity
-
+aws --profile dev s3 ls /
 pushd lambda
 ./build.sh
 popd
